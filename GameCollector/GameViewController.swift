6164e3e825867bcb7b7451e981a859ef0d38256e
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-06 00:28:19 -0700

    Only camera missing

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -10,46 +10,66 @@ import UIKit
 
 class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var addUpdateButton: UIButton!
     @IBOutlet weak var gameImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
 
     var imagePicker = UIImagePickerController()
+    var game : Game? = nil
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         imagePicker.delegate = self
+        
+        if game != nil {
+            gameImageView.image = UIImage(data: game!.image!)
+            titleTextField.text = game!.title
+            addUpdateButton.setTitle("Update", for: .normal)
+        } else {
+            deleteButton.isHidden = true
+        }
     }
 
+    
     @IBAction func photosTapped(_ sender: Any) {
-        
         imagePicker.sourceType = .photoLibrary
-        
         present(imagePicker, animated: true, completion: nil)
     }
 
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
         gameImageView.image = image
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
+    
     @IBAction func cameraTapped(_ sender: Any) {
     }
     
+    
     @IBAction func addTapped(_ sender: Any) {
-        
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-        let game = Game(context: context)
-        game.title = titleTextField.text
-        game.image = UIImagePNGRepresentation(gameImageView.image!)
-        
+        if game != nil {
+            game!.title = titleTextField.text
+            game!.image = UIImagePNGRepresentation(gameImageView.image!)
+        } else {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let game = Game(context: context)
+            game.title = titleTextField.text
+            game.image = UIImagePNGRepresentation(gameImageView.image!)
+        }
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
-    
         navigationController!.popViewController(animated: true)
+    }
     
+    
+    @IBAction func deleteTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(game!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
     }
     
 }
--- GameCollector/ViewController.swift
+++ GameCollector/ViewController.swift
@@ -14,14 +14,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var games : [Game] = []
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         tableView.dataSource = self
         tableView.delegate = self
     }
 
+    
     override func viewWillAppear(_ animated: Bool) {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
@@ -33,16 +33,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return games.count
     }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         let game = games[indexPath.row]
         cell.textLabel?.text = game.title
-        cell.imageView?.image = UIImage(data: game.image as! Data)
+        cell.imageView?.image = UIImage(data: game.image!)
         return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let game = games[indexPath.row]
+        performSegue(withIdentifier: "gameSegue", sender: game)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! GameViewController
+        nextVC.game = sender as? Game
     }
     
 }
