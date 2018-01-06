6f6e3d6fc22a1bfa4b6a129a0cb445469c08394a
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-05 22:44:43 -0700

    GameViewController set up

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Marcus Jessnitz on 05.01.18.
+//  Copyright © 2018 Marcus Jessnitz. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
