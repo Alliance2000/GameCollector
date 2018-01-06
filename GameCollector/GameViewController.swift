34bb597c966d373741591732dbe6ebf2c4fd381a
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-06 00:38:04 -0700

    Done!

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -47,6 +47,8 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
     
     
